9c2bb11e6bcdd8c731d97fb35d6844e51d593fbb
Jose Torrealba <jtorreal@uwo.ca>
2020-10-24 15:37:26 -0700

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/StickyKeys.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jtorreal/CLionProjects/StickyKeys/Bob.cpp" "/home/jtorreal/CLionProjects/StickyKeys/cmake-build-debug/CMakeFiles/StickyKeys.dir/Bob.cpp.o"
+  "/home/jtorreal/CLionProjects/StickyKeys/Draw.cpp" "/home/jtorreal/CLionProjects/StickyKeys/cmake-build-debug/CMakeFiles/StickyKeys.dir/Draw.cpp.o"
+  "/home/jtorreal/CLionProjects/StickyKeys/Engine.cpp" "/home/jtorreal/CLionProjects/StickyKeys/cmake-build-debug/CMakeFiles/StickyKeys.dir/Engine.cpp.o"
+  "/home/jtorreal/CLionProjects/StickyKeys/Input.cpp" "/home/jtorreal/CLionProjects/StickyKeys/cmake-build-debug/CMakeFiles/StickyKeys.dir/Input.cpp.o"
+  "/home/jtorreal/CLionProjects/StickyKeys/Update.cpp" "/home/jtorreal/CLionProjects/StickyKeys/cmake-build-debug/CMakeFiles/StickyKeys.dir/Update.cpp.o"
+  "/home/jtorreal/CLionProjects/StickyKeys/main.cpp" "/home/jtorreal/CLionProjects/StickyKeys/cmake-build-debug/CMakeFiles/StickyKeys.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../SFML_INCLUDE_DIR"
+  "/home/jtorreal/SFML-2.5.1-Compiled/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
