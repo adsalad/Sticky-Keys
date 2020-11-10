706acab91747e1038172c1753929a591feb98a76
Jose Torrealba <jtorreal@uwo.ca>
2020-11-09 17:06:55 -0800

    Added keyboard platform functionality and recall key

--- olcExampleProgram.cpp
+++ olcExampleProgram.cpp
@@ -23,10 +23,25 @@ private:
 	float playerVelX = 0.0f;
 	float playerVelY = 0.0f;
 
+	float mouseX = GetMouseX();
+	float mouseY = GetMouseY();
+
 	float cameraX = 0.0f;
 	float cameraY = 0.0f;
 
 	bool onGround = false;
+	bool canMoveLeft = true;
+	bool canMoveRight = true;
+	bool canJump = true;
+
+    bool leftClicked = false;
+    bool leftPlaced = false;
+    bool rightClicked = false;
+    bool rightPlaced = false;
+    bool jumpClicked = false;
+    bool jumpPlaced = false;
+
+    bool fixTiles = false;
 
 public:
 	bool OnUserCreate() override
@@ -34,6 +49,7 @@ public:
 		nLevelWidth = 64;
 		nLevelHeight = 16;
 
+        LevelOne += "................................................................";
         LevelOne += "................................................................";
         LevelOne += "................................................................";
         LevelOne += ".......#####....................................................";
@@ -49,7 +65,7 @@ public:
         LevelOne += "........................#.############......###.................";
         LevelOne += "........................#................###....................";
         LevelOne += "........................#################.......................";
-        LevelOne += "................................................................";
+
 
 	    // Called once at the start, so create things here
 		return true;
@@ -77,23 +93,38 @@ public:
 
         //Handle Input
         if(IsFocused()){
-            if(GetKey(olc::Key::UP).bPressed){
+            if(GetKey(olc::Key::UP).bPressed && canJump){
                 if(playerVelY == 0)
                     playerVelY = -12.0f;
             }
-            if(GetKey(olc::Key::DOWN).bHeld){
-                playerVelY = 6.0f;
+            if(GetKey(olc::Key::LEFT).bHeld && canMoveLeft){
+                playerVelX = -6.0f;
             }
-            if(GetKey(olc::Key::LEFT).bHeld){
-                playerVelX += (onGround ? -25.0f : -15.0f) * fElapsedTime;
+            if(GetKey(olc::Key::LEFT).bReleased){
+                playerVelX = 0.0f;
             }
-            if(GetKey(olc::Key::RIGHT).bHeld) {
-                playerVelX += (onGround ? 25.0f : 15.0f) * fElapsedTime;
+            if(GetKey(olc::Key::RIGHT).bHeld && canMoveRight) {
+                playerVelX = 6.0f;
             }
-            if(GetKey(olc::Key::SPACE).bPressed) {
+            if(GetKey(olc::Key::RIGHT).bReleased){
+                playerVelX = 0.0f;
+            }
+            if(GetKey(olc::Key::SPACE).bPressed && canJump) {
                 if(playerVelY == 0){
                     playerVelY = -12.0f;
                 }
+            }
+            if(GetKey(olc::Key::R).bPressed) {
+                canJump = true;
+                canMoveRight = true;
+                canMoveLeft = true;
+                leftPlaced = false;
+                rightPlaced = false;
+                jumpPlaced = false;
+                leftClicked = false;
+                rightClicked = false;
+                jumpClicked = false;
+                fixTiles = true;
             }
         }
 
@@ -163,7 +194,7 @@ public:
 
         //Calculate Topleft tile
         float offsetX = cameraX - (float) nVisibleTilesX / 2.0f;
-        float offsetY = cameraY - (float)nVisibleTilesY / 2.0f;
+        float offsetY = cameraY - (float) nVisibleTilesY / 2.0f;
 
         //Prevent Camera from going outside of game limits
         if(offsetX < 0) offsetX = 0;
@@ -179,6 +210,11 @@ public:
         for(int i = -1; i < nVisibleTilesX+1; i++){
             for (int j = -1; j < nVisibleTilesY+1; j++){
                 char tileID = GetTile(i + offsetX, j + offsetY);
+                if(fixTiles && (tileID == 'l' || tileID == 'r' || tileID == 'j')){
+                    tileID = '.';
+                    SetTile(i+offsetX, j+offsetY, '.');
+                }
+
                 switch (tileID)
                 {
                     case '.':
@@ -187,8 +223,94 @@ public:
                     case '#':
                         FillRect(i * nTileWidth-tileOffsetX, j * nTileHeight-tileOffsetY, nTileWidth, nTileHeight, olc::RED);
                         break;
+                    case 'l':
+                        FillRect(i * nTileWidth-tileOffsetX, j * nTileHeight-tileOffsetY, nTileWidth, nTileHeight, olc::MAGENTA);
+                    case 'r':
+                        FillRect(i * nTileWidth-tileOffsetX, j * nTileHeight-tileOffsetY, nTileWidth, nTileHeight, olc::BLUE);
+                    case 'j':
+                        FillRect(i * nTileWidth-tileOffsetX, j * nTileHeight-tileOffsetY, nTileWidth, nTileHeight, olc::CYAN);
                     default:
                         break;
+                }
+            }
+        }
+
+        if(fixTiles)
+            fixTiles = false;
+
+        //Draw Left Keyboard Key
+        int LeftKeyX = 20;
+        int LeftKeyY = 50;
+        if(leftPlaced == false && leftClicked == false){
+            FillRect(LeftKeyX, LeftKeyY, nTileWidth, nTileHeight, olc::MAGENTA);
+        }
+        if(leftPlaced == false && leftClicked == true){
+            FillRect(LeftKeyX, LeftKeyY, nTileWidth, nTileHeight, olc::DARK_MAGENTA);
+        }
+
+        //Draw Right Keyboard Key
+        int RightKeyX = LeftKeyX+nTileWidth+20;
+        int RightKeyY = 50;
+        if(rightPlaced == false && rightClicked == false){
+            FillRect(RightKeyX, RightKeyY, nTileWidth, nTileHeight, olc::BLUE);
+        }
+        if(rightPlaced == false && rightClicked == true){
+            FillRect(RightKeyX, RightKeyY, nTileWidth, nTileHeight, olc::DARK_BLUE);
+        }
+
+        //Draw Jump Keyboard Key
+        int JumpKeyX = RightKeyX+nTileWidth+20;
+        int JumpKeyY = 50;
+        if(jumpPlaced == false && jumpClicked == false){
+            FillRect(JumpKeyX, JumpKeyY, nTileWidth, nTileHeight, olc::CYAN);
+        }
+        if(jumpPlaced == false && jumpClicked == true){
+            FillRect(JumpKeyX, JumpKeyY, nTileWidth, nTileHeight, olc::DARK_CYAN);
+        }
+
+
+        //Check to see if mouse clicks on keyboard key
+        if(IsFocused()){
+            if(GetMouse(0).bPressed){
+                int clickCount = 0;
+                int mouseClickX = GetMouseX();
+                int mouseClickY = GetMouseY();
+                if(mouseClickX >= LeftKeyX && mouseClickX <= LeftKeyX + nTileWidth && mouseClickY >= LeftKeyY && mouseClickY <= LeftKeyY+nTileHeight && leftClicked == false ){
+                    leftClicked = true;
+                }
+                else if(mouseClickX >= RightKeyX && mouseClickX <= RightKeyX + nTileWidth && mouseClickY >= RightKeyY && mouseClickY <= RightKeyY+nTileHeight && rightClicked == false ){
+                    rightClicked = true;
+                }
+                else if(mouseClickX >= JumpKeyX && mouseClickX <= JumpKeyX + nTileWidth && mouseClickY >= JumpKeyY && mouseClickY <= JumpKeyY+nTileHeight && jumpClicked == false){
+                    jumpClicked = true;
+                }
+                else{
+                    if(leftClicked == true){
+                        if (GetTile((int) ((mouseClickX)/nTileWidth + offsetX), (int) ((mouseClickY)/nTileHeight) + offsetY) == '.') {
+                            SetTile((int) ((mouseClickX)/nTileWidth + offsetX), (int) ((mouseClickY)/nTileHeight) + offsetY, 'l');
+                            leftPlaced = true;
+                            canMoveLeft = false;
+                        }
+                    }
+
+                    else if(rightClicked == true){
+                        if (GetTile((int) ((mouseClickX)/nTileWidth + offsetX), (int) ((mouseClickY)/nTileHeight) + offsetY) == '.') {
+                            SetTile((int) ((mouseClickX)/nTileWidth + offsetX), (int) ((mouseClickY)/nTileHeight) + offsetY, 'r');
+                            rightPlaced = true;
+                            canMoveRight = false;
+                        }
+                    }
+
+                    else if(jumpClicked == true){
+                        if (GetTile((int) ((mouseClickX)/nTileWidth + offsetX), (int) ((mouseClickY)/nTileHeight) + offsetY) == '.') {
+                            SetTile((int) ((mouseClickX)/nTileWidth + offsetX), (int) ((mouseClickY)/nTileHeight) + offsetY, 'j');
+                            jumpPlaced = true;
+                            canJump = false;
+                        }
+                    }
+                    rightClicked = false;
+                    jumpClicked = false;
+                    leftClicked = false;
                 }
             }
         }
@@ -206,7 +328,7 @@ public:
 int main()
 {
 	Example demo;
-	if (demo.Construct(256, 240, 8, 8))
+	if (demo.Construct(256, 256, 8, 8))
 		demo.Start();
 
 	return 0;
